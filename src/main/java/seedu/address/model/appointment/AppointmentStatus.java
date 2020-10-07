27785db1afb5e2e5acd3da5a07b438e5686afd12
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-10-07 17:38:33 +0800

    initial

---
+++ src/main/java/seedu/address/model/appointment/AppointmentStatus.java
@@ -0,0 +1,38 @@
+package seedu.address.model.appointment;
+
+/**
+ * Represents the status of an Appointment.
+ */
+public enum AppointmentStatus {
+    ACTIVE("The appointment is active."),
+    CANCELLED("The appointment was cancelled."),
+    COMPLETED("The appointment was completed."),
+    MISSED("The appointment was missed.");
+
+    public static final String MESSAGE_CONSTRAINTS = "Appointment status can only be the following:\n"
+        + ACTIVE.name() + ": " + ACTIVE.message + " "
+        + CANCELLED.name() + ": " + CANCELLED.message + " "
+        + COMPLETED.name() + ": " + COMPLETED.message + " "
+        + MISSED.name() + ": " + MISSED.message;
+
+    /**
+     * The message of the appointment status.
+     */
+    private final String message;
+
+    AppointmentStatus(String message) {
+        this.message = message;
+    }
+
+    /**
+     * Returns true if a given string is a valid appointment status.
+     */
+    public static boolean isValidAppointmentStatus(String test) {
+        try {
+            AppointmentStatus appointmentStatus = AppointmentStatus.valueOf(test);
+        } catch (IllegalArgumentException e) {
+            return false;
+        }
+        return true;
+    }
+}
