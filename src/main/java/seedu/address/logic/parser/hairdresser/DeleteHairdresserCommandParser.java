9cbe2ef0fbb093d633e77d0ed1e75968e111e4da
Aloysius Lim <aloysiuslim.dw@gmail.com>
2020-10-14 20:01:46 +0800

    Make person abstract and remove associated files and methods

--- src/main/java/seedu/address/logic/parser/hairdresser/DeleteHairdresserCommandParser.java
+++ src/main/java/seedu/address/logic/parser/hairdresser/DeleteHairdresserCommandParser.java
@@ -3,7 +3,6 @@ package seedu.address.logic.parser.hairdresser;
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
 import seedu.address.commons.core.index.Index;
-import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.hairdresser.DeleteHairdresserCommand;
 import seedu.address.logic.parser.Parser;
 import seedu.address.logic.parser.ParserUtil;
@@ -21,7 +20,7 @@ public class DeleteHairdresserCommandParser implements Parser<DeleteHairdresserC
             return new DeleteHairdresserCommand(index);
         } catch (ParseException pe) {
             throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteCommand.MESSAGE_USAGE), pe);
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, DeleteHairdresserCommand.MESSAGE_USAGE), pe);
         }
     }
 
