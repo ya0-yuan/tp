16fab646951203dbf42ed410f1f51075ca174c3e
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-10-28 18:22:56 +0800

    Update TypicalAppointmentDates.java
    fix static variable not being initialized

--- src/test/java/seedu/address/testutil/TypicalAppointmentDates.java
+++ src/test/java/seedu/address/testutil/TypicalAppointmentDates.java
@@ -8,15 +8,16 @@ public class TypicalAppointmentDates {
     public static final AppointmentDate FUTURE_APPOINTMENT_DATE = getFutureDate();
     public static final String FUTURE_DATE_STRING = "2022-01-01";
     public static final String PAST_DATE_STRING = "2019-01-01";
-    private static final LocalDate currentDate = LocalDate.now();
 
     public static AppointmentDate getPastDate() {
+        final LocalDate currentDate = LocalDate.now();
         AppointmentDate pastAppointmentDate = new AppointmentDate(PAST_DATE_STRING);
         assert pastAppointmentDate.date.isBefore(currentDate);
         return pastAppointmentDate;
     }
 
     public static AppointmentDate getFutureDate() {
+        final LocalDate currentDate = LocalDate.now();
         AppointmentDate futureAppointmentDate = new AppointmentDate(FUTURE_DATE_STRING);
         assert futureAppointmentDate.date.isAfter(currentDate);
         return futureAppointmentDate;
