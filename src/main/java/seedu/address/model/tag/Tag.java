31362078aaba74d0a8caaa2af3228d9145634749
yaoyuan <yaoyuan@u.nus.edu>
2020-11-08 15:45:10 +0800

    check for no more than one input for each prefix for filter_appt method

--- src/main/java/seedu/address/model/tag/Tag.java
+++ src/main/java/seedu/address/model/tag/Tag.java
@@ -13,12 +13,7 @@ public class Tag {
             + "contain at most 20 characters.";
     public static final String VALIDATION_REGEX = "\\p{Alnum}{1,20}";
 
-<<<<<<< HEAD
-
     public final String tagName;
-=======
-    private final String tagName;
->>>>>>> master
 
     /**
      * Constructs a {@code Tag}.
