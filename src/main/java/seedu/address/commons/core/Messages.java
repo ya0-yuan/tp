fb6c2a8d654e90a49361708a0df379df2d81845f
yaoyuan <yaoyuan@u.nus.edu>
2020-11-08 15:58:41 +0800

    fix checkstyle

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -7,7 +7,8 @@ public class Messages {
 
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
-    public static final String MULTIPLE_INPUT_FOR_SAME_PREFIX = "There should be no more than one input for each prefix!";
+    public static final String MULTIPLE_INPUT_FOR_SAME_PREFIX =
+            "There should be no more than one input for each prefix!";
     public static final String MESSAGE_INVALID_CLIENT_DISPLAYED_ID = "The client ID provided is invalid";
     public static final String MESSAGE_INVALID_HAIRDRESSER_DISPLAYED_ID = "The hairdresser ID provided is invalid";
     public static final String MESSAGE_HAIRDRESSER_LISTED_OVERVIEW = "%1$d hairdressers listed!";
