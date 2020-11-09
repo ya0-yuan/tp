4f9a6f60da74fa322b215ea50a3565096810b084
nicktohzyu <nicholastohzhenyu@gmail.com>
2020-11-09 21:36:33 +0800

    fix appointmentTime 24:00 bug

--- src/test/java/seedu/address/model/appointment/AppointmentTimeTest.java
+++ src/test/java/seedu/address/model/appointment/AppointmentTimeTest.java
@@ -12,7 +12,7 @@ class AppointmentTimeTest {
     @Test
     void isValidAppointmentTime() {
         new AppointmentTime("00:00");
-        assertThrows(DateTimeParseException.class, () -> new AppointmentTime("24:00"));
+        assertThrows(IllegalArgumentException.class, () -> new AppointmentTime("24:00"));
         assertThrows(IllegalArgumentException.class, () -> new AppointmentTime("24:01"));
     }
 }
